34ba7436854b77c3749ab877a520f36594153bcc
Henrique P Martins <hpmartins@users.noreply.github.com>
2019-07-18 21:54:59 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,73 @@
-from setuptools import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-def readme():
-    with open('README') as f:
-        return f.read()
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pipenv install twine --dev
 
-setup(name='pyxro',
-      version='0.1',
-      description='Python YXRO',
-      long_description=readme(),
-      url='',
-      author='H. P. Martins',
-      author_email='hpmartins@lbl.gov',
-      license='MIT',
-      packages=['pyxro'],
-      package_dir={'pyxro': 'pyxro'},
-      #package_data={'esutils': ['data/parameters.json.gz']},
-      install_requires=[
-          'numpy',
-          'scipy',
-          'pandas',
-          'requests',
-      ],
-      include_package_data=True,
-      zip_safe=False)
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'pyxro'
+DESCRIPTION = ''
+URL = 'https://github.com/hpmartins/pyxro'
+EMAIL = 'hpmartins@lbl.gov'
+AUTHOR = 'H. P. Martins'
+REQUIRES_PYTHON = '>=3.5.0'
+VERSION = '0.1.0'
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'numpy', 'pandas', 'requests',
+]
+
+# What packages are optional?
+EXTRAS = {
+    # 'fancy feature': ['django'],
+}
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license='MIT',
+)
