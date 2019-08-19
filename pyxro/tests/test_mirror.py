178c6199cecc3feb0ad5267ca5e86a5f688eee56
Henrique P Martins <hpmartins@users.noreply.github.com>
2019-08-18 23:27:36 -0700

    Changing test and parameter files

--- pyxro/tests/test_mirror.py
+++ pyxro/tests/test_mirror.py
@@ -1,7 +1,7 @@
 import pyxro
 
 sample = pyxro.MultilayerSample()
-sample.from_parfile('test_generic.par')
+sample.from_parfile('test_mirror.par')
 
 # sample.to_parfile() -> back to old parameter
 # sample.to_json()    -> to json object
