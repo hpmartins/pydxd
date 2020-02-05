0d1cd63c84b68bc1e490a2d5ad3b2573266d5cb3
Henrique P Martins <hpmartins@gmail.com>
2020-02-05 15:38:19 -0800

    Update dtd.py
    Simplified reflectivity calculation; new variable names for Bragg conditions; automatic delta_x for x_range; fixed angle and energy offsets/ranges, and the convolution if the centralized squared mono reflectivity

--- pyxro/dtd.py
+++ pyxro/dtd.py
@@ -48,6 +48,7 @@ class Sample(object):
         all_pos = []
         for idx, sites in enumerate(self.symstruct.equivalent_sites):
             all_pos = np.append(all_pos, np.array([np.dot(self.hkl_norm, S.coords) for S in sites]))
+
         # Sets sites
         self.sites = {}
         self.zpos = {}
@@ -71,29 +72,37 @@ class Sample(object):
         c  = sp.constants.value('speed of light in vacuum')
         return h*c/(t*1e-10)
         
-    def set_mode(self, mode = 'angular', angle=0.0, energy=0.0):
+    def set_mode(self, mode = 'angular', angle=0.0, energy=0.0, polarization='sigma'):
         self.mode = mode
-
+        self.polarization = polarization
+        
         if mode == 'angular':
-            self.hv_energy = energy
-            self.hv_lambda = self.wavelength_energy_relation(self.hv_energy)
-            self.th_Bragg  = np.degrees(np.arcsin(self.hv_lambda/(2*self.d_hkl)))
+            if energy == 0.0:
+                # implementar erro
+                self.energy_Bragg = 3000
+            else:
+                self.energy_Bragg = energy
+
+            self.lambda_Bragg = self.wavelength_energy_relation(self.energy_Bragg)
+            self.theta_Bragg  = np.degrees(np.arcsin(self.lambda_Bragg/(2*self.d_hkl)))
         elif mode == 'energy':
             if angle == 0.0:
-                self.hv_energy = energy
-                self.hv_lambda = self.wavelength_energy_relation(self.hv_energy)
-                self.th_Bragg  = np.degrees(np.arcsin(self.hv_lambda/(2*self.d_hkl)))
+                self.energy_Bragg = energy
+                self.lambda_Bragg = self.wavelength_energy_relation(self.energy_Bragg)
+                self.theta_Bragg  = np.degrees(np.arcsin(self.lambda_Bragg/(2*self.d_hkl)))
             else:
-                self.th_Bragg  = angle
-                self.hv_lambda = 2*self.d_hkl*np.sin(np.radians(self.th_Bragg))
-                self.hv_energy = self.wavelength_energy_relation(self.hv_lambda)
+                self.theta_Bragg = angle
+                self.lambda_Bragg = 2*self.d_hkl*np.sin(np.radians(self.theta_Bragg))
+                self.energy_Bragg = self.wavelength_energy_relation(self.lambda_Bragg)
+
+        self.theta_Bragg_rad = np.radians(self.theta_Bragg)
     
     def calc_imfp(self):
         density = self.symstruct.density
         M_weight = self.symstruct.composition.weight
         E_g = 0.0
         N_V = 8.0
-        E_kinetic = self.hv_energy
+        E_kinetic = self.energy_Bragg
         E_p = 28.8 * np.sqrt((N_V * density) / M_weight)  
         beta = (-0.10 + (0.944 / (np.sqrt(E_p**2 + E_g**2))) + 
                (0.069 * (density**0.1))) 
@@ -133,120 +142,91 @@ class Sample(object):
         return gamma*F_0, gamma*F_H, gamma*F_Hb
 
 
-    def calc_reflectivity(self, delta=10, npts=501, Mono=False, polarization='sigma'):
-        Chi_0, Chi_H, Chi_Hb = self.calc_susceptibility(self.hv_energy)
+    def calc_reflectivity(self, delta=10, dx=0, Mono=False):
+        Chi_0, Chi_H, Chi_Hb = self.calc_susceptibility(self.energy_Bragg)
         
-        self.polarization = polarization
         if self.polarization == 'pi':
-            P = 2*np.cos(np.radians(self.th_Bragg))
+            P = 2*np.cos(np.radians(self.theta_Bragg))
         else:
             P = 1.0
             
         b = -1
         
         self.backscattering_angle = np.degrees(np.pi/2 - 2*np.abs(Chi_0))
-        self.angleshift = np.real(Chi_0)/np.sin(2*np.radians(self.th_Bragg))
-        self.energyshift = self.hv_energy*self.angleshift
         
-        if self.th_Bragg < self.backscattering_angle:
-            self.anglewidth = 2*np.abs(P)*np.abs(np.sqrt(Chi_H*Chi_Hb))/(np.sqrt(np.abs(b))*np.sin(2*np.radians(self.th_Bragg)))
-        else:
-            self.anglewidth = np.sqrt(2*np.abs(np.sqrt(Chi_H*Chi_Hb)))
-        
-        self.energywidth = self.hv_energy*np.abs(P)*np.abs(np.sqrt(Chi_H*Chi_Hb))/(np.sin(np.radians(self.th_Bragg)))**2
+        self.angle_offset = np.degrees(np.real(Chi_0)/np.sin(2*self.theta_Bragg_rad))
+        self.angle_range  = np.degrees(np.abs(P)*np.sqrt(np.abs(Chi_H*Chi_Hb))/np.sin(2*self.theta_Bragg_rad))
+        self.energy_offset = np.real(Chi_0)*self.energy_Bragg/(2*np.sin(self.theta_Bragg_rad)**2)
+        self.energy_range  = self.energy_Bragg*np.abs(P)*np.sqrt(np.abs(Chi_H*Chi_Hb))/(2*np.sin(self.theta_Bragg_rad)**2)
         
         if self.mode == 'angular':
+            if dx == 0:
+                dx = 0.001
+            npts = int(2*delta/dx)+1
             x_range = np.linspace(-delta, delta, npts)
+            
+            xarg = np.radians(x_range)
+            targ = np.radians(self.theta_Bragg)
+            if self.theta_Bragg < self.backscattering_angle:
+                first_term = b*xarg*np.sin(2*targ)
+            else:
+                first_term = np.sqrt(1 + 2*xarg**2 * (np.sin(targ))**2 + 2*b*xarg*np.sin(2*targ)) - 1
         elif self.mode == 'energy':
+            if dx == 0:
+                dx = 0.01
+            npts = int(2*delta/dx)+1
             x_range = np.linspace(-delta, delta, npts)
             
-        # if self.mode == 'angular':
-        #     angle_range = 10*np.degrees(self.anglewidth)
-        #     x_range = np.r_[self.angleshift - angle_range:
-        #                     self.angleshift + angle_range:
-        #                         1j*npts]
-        # elif self.mode == 'energy':
-        #     energy_range = 10*self.energywidth
-        #     x_range = np.r_[self.energyshift - energy_range:
-        #                     self.energyshift + energy_range:
-        #                         1j*npts]
+            xarg = x_range/(x_range + self.energy_Bragg)
+            targ = np.radians(self.theta_Bragg)
+            first_term = 2*b*xarg*(np.sin(targ))**2
+             
+        eta = (first_term + ((1-b)/2)*Chi_0) / (np.absolute(P)*np.sqrt(np.absolute(b)*Chi_H*Chi_Hb))
+    
+        prefactor = np.sqrt(np.abs(b)) * (np.abs(P)/P) * np.sqrt(Chi_H/Chi_Hb)
+        EHE0 = -prefactor*np.piecewise(eta, np.real(eta) > 0,
+                 [lambda x: x-(x**2-1)**.5, lambda x: x+(x**2-1)**.5])
+        
+        Refl = np.abs(EHE0)**2
+        Phase = np.remainder(np.angle(EHE0) + np.pi/2, 2*np.pi) - np.pi/2
 
-        Refl  = np.zeros_like(x_range)
-        Phase = np.zeros_like(x_range)
-        
-        for i, x in enumerate(x_range):
-            if self.mode == 'energy':
-                # Chi_0, Chi_H, Chi_Hb = self.calc_susceptibility(x + self.hv_energy)
-                xarg = x/(x + self.hv_energy)
-                targ = np.radians(self.th_Bragg)
-                first_term = 2*b*xarg*(np.sin(targ))**2
-            elif self.mode == 'angular':
-                xarg = np.radians(x)
-                targ = np.radians(self.th_Bragg)
-                if self.th_Bragg < self.backscattering_angle:
-                    first_term = b*xarg*np.sin(2*targ)
-                else:
-                    first_term = np.sqrt(1 + 2*xarg**2 * (np.sin(targ))**2 + 2*b*xarg*np.sin(2*targ)) - 1
-                
-            eta = (first_term + ((1-b)/2)*Chi_0) / (np.absolute(P)*np.sqrt(np.absolute(b)*Chi_H*Chi_Hb))
-        
-            prefactor = np.sqrt(np.abs(b)) * (np.abs(P)/P) * np.sqrt(Chi_H/Chi_Hb)
-            EHE0 = -prefactor*np.piecewise(eta, np.real(eta) > 0,
-                     [lambda x: x-(x**2-1)**.5, lambda x: x+(x**2-1)**.5])
-            
-            Refl[i] = np.abs(EHE0)**2
-            Phase[i] = np.remainder(np.angle(EHE0) + np.pi/2, 2*np.pi) - np.pi/2
-            
-            # EHE0_plus  = -1*(P/np.abs(P))*(eta+np.sqrt(eta**2-1))*np.sqrt(np.absolute(b)*Chi_H/Chi_Hb)
-            # EHE0_minus = -1*(P/np.abs(P))*(eta-np.sqrt(eta**2-1))*np.sqrt(np.absolute(b)*Chi_H/Chi_Hb)
-            # phi_plus   = np.arctan(np.imag(EHE0_plus )/ np.real(EHE0_plus ))
-            # phi_minus  = np.arctan(np.imag(EHE0_minus)/ np.real(EHE0_minus))
-            # refl_plus  = np.abs(EHE0_plus )**2
-            # refl_minus = np.abs(EHE0_minus)**2
-        
-            # if refl_plus>0 and refl_plus<1:
-            #     Refl[i]  = refl_plus
-            #     Phase[i] = phi_plus
-            # else:
-            #     Refl[i]  = refl_minus
-            #     Phase[i] = phi_minus
-        
-            #     if np.real(EHE0_minus) < 0 and self.polarization == 'sigma':
-            #         Phase[i] += np.pi
-            #     elif np.real(EHE0_minus) > 0 and self.polarization == 'pi':
-            #        Phase[i] += np.pi
-            #     else:
-            #        pass
-               
-                
         self.x_range = x_range
         self.Refl = Refl
         self.Phase = Phase
         self.Mono = Mono
         
         if self.Mono:
-            self.Mono.calc_reflectivity(delta=delta, npts=npts)
-            self.Mono.Squared_Refl = self.Mono.Refl**2/np.sum(self.Mono.Refl**2)
-            self.Refl_conv_Mono = np.convolve(self.Refl, self.Mono.Squared_Refl, mode='full')
-            self.Phase_conv_Mono = np.convolve(self.Phase, self.Mono.Squared_Refl, mode='full')
+            self.Mono.set_mode(self.mode, energy=self.energy_Bragg)
+            self.Mono.calc_reflectivity(delta=delta, dx=dx)
             
-            tmp = np.linspace(-2*delta, 2*delta, 2*npts-1)
-            self.Refl_conv_Mono = np.interp(self.x_range, tmp, self.Refl_conv_Mono)
-            self.Phase_conv_Mono = np.interp(self.x_range, tmp, self.Phase_conv_Mono)
+            shift_sample = (self.angle_offset if self.mode == 'angular' else self.energy_offset)
+            shift_mono = (self.Mono.angle_offset if self.mode == 'angular' else self.Mono.energy_offset)
+
+            self.Refl_shifted = np.interp(self.x_range, self.x_range - shift_sample, self.Refl)
+            self.Phase_shifted = np.interp(self.x_range, self.x_range - shift_sample, self.Phase)
+            self.Mono.Refl_shifted = np.interp(self.Mono.x_range, self.Mono.x_range - shift_mono, self.Mono.Refl)
+            
+            self.Mono.Squared_Refl = self.Mono.Refl_shifted**2/np.sum(self.Mono.Refl_shifted**2)
+            self.Refl_conv_Mono = np.correlate(self.Refl, self.Mono.Squared_Refl, mode='same')
+            self.Phase_conv_Mono = np.correlate(self.Phase, self.Mono.Squared_Refl, mode='same')
     
-    def calc_sw_cohpos(self, cohfra, cohpos):
+    def calc_sw_cohpos(self, cohfra, cohpos, Q = 0.0, Delta = 0.0):
         if self.Mono:
             R = self.Refl_conv_Mono
             P = self.Phase_conv_Mono
         else:
             R = self.Refl
             P = self.Phase
+            
+        S_R = (1 + Q)/(1 - Q)
+        S_I = np.sqrt(1 + (Q*np.tan(np.radians(Delta)))**2)/(1 - Q)
+        Psi = np.arctan(Q*np.tan(np.radians(Delta)))
         
-        return 1 + self.S_R*R + 2*np.abs(self.S_I)*np.sqrt(R)*cohfra*np.cos(P - 2*np.pi*cohpos + self.Psi)
+        return 1 + S_R*R + 2*np.abs(S_I)*np.sqrt(R)*cohfra*np.cos(P - 2*np.pi*cohpos + Psi)
     
     def calc_yield(self):
         self.calc_imfp()
-        lambda_IMFP = self.imfp * np.sin(np.radians(self.th_Bragg))
+        lambda_IMFP = self.imfp * np.sin(np.radians(self.theta_Bragg))
 
         Y = {}
         for idx, sites in enumerate(self.symstruct.equivalent_sites):
@@ -329,13 +309,13 @@ def plot_Refl_and_Phase(Sample, colors=None, trim=0, mode='relative'):
             ax3.set_xlabel('Relative photon energy (eV)')
         else:
             ax3.set_xlabel('Photon energy (eV)')
-        plt.title('E$_B$ = {:.2f} eV @ {:.2f} deg'.format(Sample.hv_energy, Sample.th_Bragg))
+        plt.title('E$_B$ = {:.2f} eV @ {:.2f} deg'.format(Sample.energy_Bragg, Sample.theta_Bragg))
     elif Sample.mode == 'angular':
         if mode == 'relative':
             ax3.set_xlabel(r'Relative incident angle ($\degree$)')
         else:
             ax3.set_xlabel(r'Incident angle ($\degree$)')
-        plt.title(r'$\theta_B$ = {:.2f}$\degree$ @ {:.2f} eV'.format(Sample.th_Bragg, Sample.hv_energy))
+        plt.title(r'$\theta_B$ = {:.2f}$\degree$ @ {:.2f} eV'.format(Sample.theta_Bragg, Sample.energy_Bragg))
         
     ax3.set_ylabel('Photoemission yield')
     
@@ -349,9 +329,9 @@ def plot_Refl_and_Phase(Sample, colors=None, trim=0, mode='relative'):
     
     if mode == 'absolute':
         if Sample.mode == 'energy':
-            xshift = Sample.hv_energy
+            xshift = Sample.energy_Bragg
         elif Sample.mode == 'angular':
-            xshift = Sample.th_Bragg
+            xshift = Sample.theta_Bragg
     elif mode == 'relative':
         xshift = 0.0
         
@@ -439,10 +419,10 @@ def plot_Electric_Field(Sample, trim=0, colors=None):
 
     if Sample.mode == 'energy':
         ax[0].set_xlabel(r'Relative photon energy (eV)')
-        ax[0].set_title('E$_B$ = {:.2f} eV @ {:.2f}$\degree$'.format(Sample.hv_energy, Sample.th_Bragg))
+        ax[0].set_title('E$_B$ = {:.2f} eV @ {:.2f}$\degree$'.format(Sample.energy_Bragg, Sample.theta_Bragg))
     elif Sample.mode == 'angular':
         ax[0].set_xlabel(r'Relative incident angle ($\degree$)')
-        ax[0].set_title(r'$\theta_B$ = {:.2f}$\degree$ @ {:.2f} eV'.format(Sample.th_Bragg, Sample.hv_energy))
+        ax[0].set_title(r'$\theta_B$ = {:.2f}$\degree$ @ {:.2f} eV'.format(Sample.theta_Bragg, Sample.energy_Bragg))
 
     ax[0].set_ylabel('Depth ($\AA$)')
     ax[0].set_xlim(Sample.x_range[0] + trim, Sample.x_range[-1] - trim)
